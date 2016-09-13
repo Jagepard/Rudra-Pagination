6dae02c369dd565411e91c878e78185e9a7d0d9d
Jagepard <dankorot@gmail.com>
2016-09-13 18:21:55 +0300

    Добавил свойства и геттеры сеттеры

--- src/Pagination.php
+++ src/Pagination.php
@@ -3,13 +3,13 @@
 namespace Rudra;
 
 
-/**
- * Date: 13.09.16
- * Time: 13:07
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2016, Korotkov Danila
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
- */
+    /**
+     * Date: 13.09.16
+     * Time: 13:07
+     * @author    : Korotkov Danila <dankorot@gmail.com>
+     * @copyright Copyright (c) 2016, Korotkov Danila
+     * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+     */
 
 /**
  * Class Pagination
@@ -17,4 +17,109 @@ namespace Rudra;
  */
 class Pagination
 {
+    /**
+     * @var
+     */
+    protected $page;
+
+    /**
+     * @var
+     */
+    protected $perPage;
+
+    /**
+     * @var
+     */
+    protected $count;
+
+    /**
+     * @var
+     */
+    protected $offset;
+
+    /**
+     * @var
+     */
+    protected $total;
+
+    /**
+     * Pagination constructor.
+     * @param $value
+     */
+    public function __construct($value)
+    {
+        $this->page = $value['page'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPerPage()
+    {
+        return $this->perPage;
+    }
+
+    /**
+     * @param mixed $perPage
+     */
+    public function setPerPage($perPage)
+    {
+        $this->perPage = $perPage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+    /**
+     * @param mixed $count
+     */
+    public function setCount($count)
+    {
+        $this->count = $count;
+    }
+
+    /**
+     * @param mixed $offset
+     */
+    public function setOffset($offset)
+    {
+        $this->offset = $offset;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOffset()
+    {
+        return $this->getPage() * $this->getPerPage() - $this->getPerPage();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * @param mixed $total
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+    }
 }
