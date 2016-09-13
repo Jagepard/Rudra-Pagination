8f2e8e811318dd1a4edecd8fcc1a95ff7ef5e691
Jagepard <dankorot@gmail.com>
2016-09-13 17:31:05 +0300

    first commit

---
+++ src/Pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Rudra;
+
+
+/**
+ * Date: 13.09.16
+ * Time: 13:07
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2016, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+/**
+ * Class Pagination
+ * @package Rudra
+ */
+class Pagination
+{
+}
